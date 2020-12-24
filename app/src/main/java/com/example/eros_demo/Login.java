2ab1c2d407a715cd3b32b0e900861cf10b8ddc6d
=pa1012 <=phuonganht1012@gmail.com>
2020-12-24 15:53:10 +0700

    login and profile fragment

---
+++ app/src/main/java/com/example/eros_demo/Login.java
@@ -0,0 +1,23 @@
+package com.example.eros_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Login extends AppCompatActivity {
+    ImageView facebook_login, gmail_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initComponent();
+    }
+
+    private void initComponent() {
+        facebook_login = findViewById(R.id.iv_facebook_login);
+        gmail_login = findViewById(R.id.iv_gmail_login);
+    }
+}
