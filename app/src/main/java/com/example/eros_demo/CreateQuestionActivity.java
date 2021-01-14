1a4742b1aa23ba985b86fbfcad7eaf902ae5aea6
Thanh Dat <thanhdat02032000@gmail.com>
2021-01-14 10:12:22 +0700

    update main screen

---
+++ app/src/main/java/com/example/eros_demo/CreateQuestionActivity.java
@@ -0,0 +1,14 @@
+package com.example.eros_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_question);
+    }
+}
